67d7e4cc870e67bf6f052be8cb7c65e8e38958a6
bakesbyish <ishani@bakesbyish.com>
2024-01-11 20:59:49 +0530

    Added various measurement units

---
+++ lib/measurement.ts
@@ -0,0 +1,32 @@
+export default [
+	{
+		title: "Pieces",
+		type: "string",
+		value: "pcs",
+	},
+	{
+		title: "Kilogram",
+		type: "string",
+		value: "kg",
+	},
+	{
+		title: "Grams",
+		type: "string",
+		value: "gram",
+	},
+	{
+		title: "Yards",
+		type: "string",
+		value: "y",
+	},
+	{
+		title: "Meters",
+		type: "string",
+		value: "m",
+	},
+	{
+		title: "Foot",
+		type: "string",
+		value: "ft",
+	},
+];
