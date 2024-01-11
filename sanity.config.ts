a068aeadd5f40bdf3e5f427809cf7686e5da3f12
bakesbyish <ishani@bakesbyish.com>
2024-01-11 23:08:58 +0530

    Added sperators to logic docs and non logi docs

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,18 +1,39 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
+import { colorInput } from "@sanity/color-input";
 
 export default defineConfig({
-  name: 'default',
-  title: 'bakesbyish',
+	name: "default",
+	title: "bakesbyish",
 
-  projectId: '7p2xutc1',
-  dataset: 'production',
+	projectId: "7p2xutc1",
+	dataset: "production",
 
-  plugins: [structureTool(), visionTool()],
+	plugins: [
+		structureTool({
+			structure: (S) => {
+				return S.list()
+					.title("Content")
+					.items([
+						...S.documentTypeListItems().filter(
+							(item) =>
+								item.getId() !== "colors" && item.getId() !== "varients",
+						),
+						S.divider(),
+						...S.documentTypeListItems().filter(
+							(item) =>
+								item.getId() !== "products" && item.getId() !== "catergories",
+						),
+					]);
+			},
+		}),
+		visionTool(),
+		colorInput(),
+	],
 
-  schema: {
-    types: schemaTypes,
-  },
-})
+	schema: {
+		types: schemaTypes,
+	},
+});
